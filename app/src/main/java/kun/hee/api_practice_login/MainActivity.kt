b0603325859c7c3d60837244e8026ac4242d189e
smlrkh1107 <krae7789230@gmail.com>
2020-04-23 21:50:59 +0900

    로그인 화면 구현 / 이벤트 처리 기초

--- app/src/main/java/kun/hee/api_practice_login/MainActivity.kt
+++ app/src/main/java/kun/hee/api_practice_login/MainActivity.kt
@@ -2,6 +2,7 @@ package kun.hee.api_practice_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -12,7 +13,12 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        loginBtn.setOnClickListener {
+            val inputId = idEdt.text.toString() //아이디가 존재하지 않는다면 ?
+            val inputPw = pwEdt.text.toString() //비밀번호가 틀리다면 ?
         }
+
+    }
 
     override fun setValues() {
 
